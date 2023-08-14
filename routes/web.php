ae2149ddc176cb3e8383011281a833042b9d0472
Richardson Oge <richardsonoge34@gmail.com>
2023-08-14 17:12:17 -0400

    Move static pages to pages folder and remove link from about page

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('', function () {
+    return view('pages.home');
 });
 
-Route::get('/about-us', function() {
-    return view('aboutus');
+Route::get('about-us', function() {
+    return view('pages.aboutus');
 });
