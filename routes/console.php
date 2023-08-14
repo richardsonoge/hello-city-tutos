ba4a0c1b3e3db5ed20ed0e68c37e715c1455a97a
Richardson Oge <richardsonoge34@gmail.com>
2023-08-14 15:06:42 -0400

    Update README and homepage

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('toto', function () {
+    $this->comment('Toto est cool!');
+})->purpose('Notre commande Toto.');
