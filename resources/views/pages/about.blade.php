69107a7dfc2f0d7a8e27b90cee57a2dca4dccc5f
Richardson Oge <richardsonoge34@gmail.com>
2023-08-14 19:26:56 -0400

    Add proper page titles

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,7 @@
 @extends('app')
 
+@section('title', 'About Us | Hello city')
+
 @section('section')
     <p>
         Built with &hearts; by LES TEACHERS DU NET.   
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,7 @@
 @extends('app')
 
+@section('title', 'Hello city')
+
 @section('section')
     <h1>Hello from Port-au-Prince</h1>
     <p>It's currently {{ date('h:i A') }}.</p>
