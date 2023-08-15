d2b4bea22396302171be07895cf020f328387e51
Richardson Oge <richardsonoge34@gmail.com>
2023-08-15 18:12:08 -0400

    Add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'About Us | '.config('app.name'))
 
 @section('content')
+    <img src="images/Beautiful-sky-photography-lisa-fotios.jpeg" width="200px" alt="Beautiful-sky-photography-lisa-fotios">
     <p>
         Built with &hearts; by LES TEACHERS DU NET.   
     </p>
