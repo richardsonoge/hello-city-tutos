0b035f67de4803826144aa07e2ca6074b1ebc626
Richardson Oge <richardsonoge34@gmail.com>
2023-08-15 19:46:16 -0400

    Make use of asset function help to load images

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,7 @@
 @section('title', config('app.name'))
 
 @section('content')
-    <img src="images/all-devices-white.png" width="200px" alt="all-devices-white">
+    <img src="{{ asset('images/all-devices-white.png') }}" width="200px" alt="all-devices-white">
     <h1>Hello from Port-au-Prince</h1>
     <p>It's currently {{ date('h:i A') }}.</p>
 @endsection
